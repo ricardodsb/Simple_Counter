507ff25ce24ff5552f396e170c5d4ff265336663
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-17 22:37:45 +0000

    done

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 export const SimpleCounter = props => {
 	return (
-		<div className="bigCounter">
+		<div className="bigCounter" style={{ alignItems: "center" }}>
 			<div className="clock">
 				<img
 					src="https://icon-library.com/images/white-clock-icon/white-clock-icon-16.jpg"
-					style={{ width: "35px" }}></img>
+					style={{
+						width: "50px",
+						borderRight: "50px"
+					}}></img>
 			</div>
 			<div className="four">{Math.floor(props.time / 1000)}</div>
 			<div className="three">{Math.floor((props.time % 1000) / 100)}</div>
